73d8d794552441e4e360b8986f7f87edcb0cb181
akshatra12 <150356757+akshatra12@users.noreply.github.com>
2025-10-08 13:08:10 +0530

    Fixed

--- app.py
+++ app.py
@@ -672,3 +672,5 @@ if __name__ == '__main__':
     # Production-ready server
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port, debug=False)
+
+application = app
